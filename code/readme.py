a18b306945c8fbccaa882f87634cc8a3799ad6f2
SarahPavlak <46604461+SarahPavlak@users.noreply.github.com>
2019-04-05 14:34:17 -0400

    Create readme.py

---
+++ code/readme.py
@@ -0,0 +1,20 @@
+#installations:
+    #tinketer: Running python -m tkinter from the command line should open a window demonstrating a simple Tk interface, letting you know that tkinter is properly installed on your system, and also showing what version of Tcl/Tk is installed, so you can read the Tcl/Tk documentation specific to that version.
+    #https://stackoverflow.com/questions/45441885/how-can-i-create-a-dropdown-menu-from-a-list-in-tkinter/45442534#45442534
+
+#Installing the google sheets API
+    #https://developers.google.com/sheets/api/quickstart/python?refresh=1
+    #install to terminal: pip install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib
+    #pip install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib
+    #https://console.developers.google.com/apis/api/sheets.googleapis.com/landing?project=genial-airway-236718
+    #https://developers.google.com/sheets/api/quickstart/python
+    #press allow https://accounts.google.com/o/oauth2/auth?response_type=code&client_id=703740991941-di7q4ci2rdt3ob9lp8bqre7ljf2ud9s4.apps.googleusercontent.com&redirect_uri=http%3A%2F%2Flocalhost%3A8080%2F&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fspreadsheets.readonly&state=xkgaJYnzWkud6arSXNC9PrwuzbDfu3&access_type=offline
+
+
+#App startup
+    #on your command line: heroku login
+    #heroku create name
+
+#Beautiful Soup
+    #$ pip install beautifulsoup4
+    #pip install requests 
